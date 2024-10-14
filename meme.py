702a8ad62e2cd2e10c7292a94f6bcd3120cb88cb
KaanAydin54 <kaanaydin8754@gmail.com>
2024-10-14 20:30:30 +0300

    Create meme.py

---
+++ meme.py
@@ -0,0 +1,13 @@
+meme_dict = { 
+      "Cringe": "Garip yada utandırıcı durumlar",
+        "LOL": "Komik durumlara verilen tepki",
+            "Sus": "Şüpheli",
+        "troll": "Alay durumlarında kullanılır"
+        }
+word = input("Anlamını öğrenmek istediğiniz kelimeyi girin:")
+
+if word in meme_dict.keys():
+    print("Aradığınız kelimenin anlamı", meme_dict(word))
+
+else: 
+    print("Maalesef aradığınız kelimeyi bilmiyorum...")
